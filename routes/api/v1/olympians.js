47f5a60f21636614d4ad3a51bd80413b403b17ca
nancylee713 <srlee713@gmail.com>
2020-01-12 19:27:39 -0700

    Add get olympian stats endpoint with passing test

--- routes/api/v1/olympians.js
+++ routes/api/v1/olympians.js
@@ -33,4 +33,25 @@ router.get('/olympians', (request, response) => {
     }
 });
 
+router.get('/olympian_stats', async (request, response) => {
+  let temp = await Promise.all(olympian.stats())
+    .then((data) => {
+      let result = {
+                    "olympian_stats": {
+                      "total_competing_olympians": data[0],
+                      "average_weight": {
+                        "unit": "kg",
+                        "male_olympians": Number(data[1].toFixed(1)),
+                        "female_olympians": Number(data[2].toFixed(1))
+                      },
+                      "average_age": Number(data[3].toFixed(1))
+                    }
+                  }
+      return response.status(200).json(result);
+    })
+    .catch((error) => {
+      return response.status(500).json({ error });
+    });
+});
+
 module.exports = router;
